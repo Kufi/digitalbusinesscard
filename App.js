dc8da34a1d36d0244c217e700b1ce2fbfd8ae501
Daniel Grant <69221595+Kufi@users.noreply.github.com>
2023-03-10 15:41:50 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import About from "./components/About"
+import Footer from "./components/Footer"
+import Interests from "./components/Interests"
+import Info from "./components/Info"
+
+function App(){
+    return (
+        <main>
+            <Info />
+            <About />
+            <Interests />
+            <Footer />
+        </main>
+    )
+}
+
+export default App
